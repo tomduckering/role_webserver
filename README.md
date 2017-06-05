eda608ba59fad22e2e481905ba46595d3fa11de8
Tom Duckering <tom.duckering@gmail.com>
2017-06-05 11:57:23 +0100

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# role_webserver
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: role_webserver
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
